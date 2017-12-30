8338b32bd01b909e3f9e92aa6b96fd5e9e84d347
Hasan Khan <hasan.khan.uap@gmail.com>
2017-12-30 15:16:37 +0600

    Create stack.py

---
+++ Data Structures/Code/stack.py
@@ -0,0 +1,11 @@
+# Stack example (Last In First Out)
+stack = [1, 2, 3, 1, 4, 5, 6]
+print(stack)
+
+# Element push at the end in list (Last In) (Push method)
+stack.append(7)
+print(stack)
+
+# Last element removed from the list (First Out) (Pop method)
+stack.pop()
+print(stack)
