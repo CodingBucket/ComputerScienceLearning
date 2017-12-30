acb5a048f5ff609c59fc81fda03271310090118b
Hasan Khan <hasan.khan.uap@gmail.com>
2017-12-30 15:25:01 +0600

    Create queue.py

---
+++ Data Structures/Code/queue.py
@@ -0,0 +1,12 @@
+# Queue example (First In First Out) using Push and Pop method
+queue = []
+print(queue)
+
+# Element adding in the list
+queue.append(1)
+queue.append(2)
+print(queue)
+
+# First element removed from the list (First Out)
+queue.pop(0)
+print(queue)
