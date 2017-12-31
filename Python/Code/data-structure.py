70f9f3b419ede7c094721a6a43e25e7641a28dd3
Hasan Ahmed Khan <hasan.khan.uap@gmail.com>
2018-01-01 00:11:18 +0600

    data-structure

---
+++ Python/Code/data-structure.py
@@ -0,0 +1,14 @@
+# List data structure
+li = [1, 2, 3]
+print(li)
+print(type(li))
+
+# Tuple data structure
+tpl = (1, 2, 3)
+print(tpl)
+print(type(tpl))
+
+# Set data structure
+st = {1, 2, 3}
+print(st)
+print(type(st))
