3fd90d92c3852a4059816bc46655ddb039597e10
Hasan Khan <hasan.khan.uap@gmail.com>
2018-01-08 23:09:38 +0600

    Update 004_Array.cs

--- C#/Code/004_Array.cs
+++ C#/Code/004_Array.cs
@@ -6,17 +6,20 @@ class Program
     {
         // int array
         int[] num = { 1, 2, 3 };
-        foreach(int n in num)
-            Console.WriteLine(n);
 
         int[] num1 = new int[3];
         num1[0] = 11;
         num1[1] = 12;
         num1[2] = 13;
-        foreach (int n in num1)
-            Console.WriteLine(n);
 
-        Console.ReadKey();
+        // string array
+        string[] str1 = { "red", "blue", "green", "yellow" };
+
+        string[] str2 = new string[] { "red", "blue", "green", "yellow" };
+
+        string[] str3 = new string[4];
+        str3[0] = "item1";
+        str3[1] = "item2";
     }
 }
 
