2b676f62b1b61cc90b54bc66dcccff79ed6754af
Hasan Khan <hasan.khan.uap@gmail.com>
2018-01-12 21:57:10 +0600

    Update 007_Class.cs

--- C#/Code/007_Class.cs
+++ C#/Code/007_Class.cs
@@ -4,21 +4,33 @@ class Program
 {
     public static void Main(string[] arg)
     {
-        Demo pro = new Demo("Hasan", "Khan");
-
+        Customer cus = new Customer("Hasan", "Khan");
+        cus.PrintFullName();
         Console.ReadKey();
     }
 }
 
-public class Demo
+public class Customer
 {
     public string _firstName;
     public string _lastName;
 
-    public Demo(string firstName, string lastName)
+    public Customer(string firstName, string lastName)
     {
         _firstName = firstName;
         _lastName = lastName;
+    }
+
+    public void PrintFullName()
+    {
+        string fullName = _firstName + " " + _lastName;
+        Console.WriteLine("Full name is {0}", fullName);
+    }
+
+    // Destructor 
+    ~Customer()
+    {
+        // cleanup code goes here
     }
 }
 
