186a86a78efefa8daa1a4b1828429e05340a7f6e
Hasan Khan <hasan.khan.uap@gmail.com>
2018-03-10 16:31:20 +0600

    Update 021_Enum.cs

--- C#/Code/021_Enum.cs
+++ C#/Code/021_Enum.cs
@@ -5,8 +5,17 @@ public class Program
     public static void Main(string[] arg)
     {
         CarType car = CarType.Toyota;
-
         Console.WriteLine(car);
+
+        // Get all enum values
+        int[] values = (int[])Enum.GetValues(typeof(CarType));
+        foreach (var val in values)
+            Console.WriteLine(val);
+
+        // Get all enum names
+        string[] names = Enum.GetNames(typeof(CarType));
+        foreach (var name in names)
+            Console.WriteLine(name);  
 
         Console.ReadKey();
     }
